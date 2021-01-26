a3b2136702dad226489c8d181f780b4098f92834
raghad <raghad.hav@gmail.com>
2021-01-27 00:10:03 +0300

    updated index file

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-require('dotenv').config();
-const express = require('express');
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
-const app = express();
-app.use(express.json());
-const cors = require("cors");
-app.use(cors());
 
-app.get('/',(req,res, next) => {
-res.send('the main page')
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
 
-app.post('',() => {
+const Blog = mongoose.model('Blog', blogSchema)
 
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        console.log(`successful connection `)
+    })
+    .catch(error => {
+        console.log(`failed to connect ${error}`)
+    });
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
-// const PORT = process.env.PORT; 
-const PORT = 3004;
-app.listen(PORT,() => {
-    console.log(`server running on port ${PORT}`)
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
 
-
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
