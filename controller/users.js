5c12f6720b46441e3b24c5be55486dcba61f1e17
Raghad Hav <raghad.hav@gmail.com>
2023-10-31 21:09:30 +0000

    remove unused code

--- controller/users.js
+++ controller/users.js
@@ -1,33 +1,14 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 // const { request } = require('express')
 // const { response } = require('../app')
-const User = require('../models/user')
-const Blogs = require('../models/blogData')
+const User = require("../models/user");
+const Blogs = require("../models/blogData");
 
-const usersRouter = require('express').Router()
+const usersRouter = require("express").Router();
 
-// regsiter user
-usersRouter.post('/', async (request, response) => {
-  const body = request.body
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", { title: 1 });
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
-
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
-  const savedUser = await user.save()
-
-  response.json(savedUser)
-})
-
-// login user
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  .populate('blogs',{title: 1})
-
-  response.json(users)
-})
-module.exports = usersRouter
+  response.json(users);
+});
+module.exports = usersRouter;
